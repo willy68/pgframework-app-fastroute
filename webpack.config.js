f60f4abf516016effc0d277a2fd7451a1e4b2b92
William Lety <william.lety@gmail.com>
2021-02-11 18:05:28 +0100

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ Encore
     //.addEntry('page2', './assets/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    //.splitEntryChunks()
+    .splitEntryChunks()
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
